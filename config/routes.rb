9a7037aca488bfe928908224557cec403dfdb18b
West Kraemer <WestKraemer@gmail.com>
2015-10-18 18:13:50 -0400

    added pics and html css styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'welcome/homepage'
+
   resources :reviews
   resources :products
+  # resources :home
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#homepage'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
